663b979bfb9e4912fc2d2d4a7fa5aa8cebc7cebc
葛尧 <geyao2012@gmail.com>
2016-01-23 23:40:23 +0800

    1.23
    NEW TRIP

---
+++ Hello.java
@@ -0,0 +1,17 @@
+package hello;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("hello world");
+//        while(true){
+//        Scanner in =new Scanner(System.in);
+//        System.out.println("Cortana:"+in.nextLine());
+//        }
+        System.out.println(2+3+"=2+3="+5+"="+(2+3));
+	}
+
+}
