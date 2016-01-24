a16a7cba3076ff510c934cd351d5e1c57078140a
葛尧 <geyao2012@gmail.com>
2016-01-24 09:56:15 +0800

    1/24

--- Hello.java
+++ Hello.java
@@ -18,8 +18,18 @@ public class Hello {
 //        System.out.println("100-"+a+"="+(100-a));
 		
 		/*第二个小程序*/
-		int price;
+//		int price;
 //		System.out.println(price);不能输出 因为没有初始化
+		int feet,inch;
+		System.out.print("输入英尺数");
+		Scanner in=new Scanner(System.in);
+		feet=in.nextInt();
+//		System.out.println(feet);
+		System.out.print("请输入英寸数");
+		inch=in.nextInt();
+//		System.out.println(inch);
+		System.out.println("换成公制身高为"+(int)((feet+inch/12.0)*0.3080*100));
+		//这里用12.0是因为二者必须有一个浮点数，结果才为浮点数
 	}
 
 }
