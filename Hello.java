90e461c3706dcbb9523bf8047f3a9c9ee6afe0aa
葛尧 <geyao2012@gmail.com>
2016-01-24 09:20:48 +0800

    1/24
    1/24

--- Hello.java
+++ Hello.java
@@ -6,12 +6,20 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("hello world");
-//        while(true){
+//		// JAVA第一个小程序
+//        System.out.println("hello world");
+////        while(true){
 //        Scanner in =new Scanner(System.in);
+//        int a=in.nextInt();//看来语言和语言都是相通的！！！
+//
 //        System.out.println("Cortana:"+in.nextLine());
-//        }
-        System.out.println(2+3+"=2+3="+5+"="+(2+3));
+////        }
+//        System.out.println(2+3+"=2+3="+5+"="+(2+3));
+//        System.out.println("100-"+a+"="+(100-a));
+		
+		/*第二个小程序*/
+		int price;
+//		System.out.println(price);不能输出 因为没有初始化
 	}
 
 }
