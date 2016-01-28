1db8cb9ca2c87b83fcb9066d361f08342219b045
葛尧 <geyao2012@gmail.com>
2016-01-28 21:16:22 +0800

    可扩展性
    用.length这样的方法写出的代码具有可扩展性

--- 非传统法找素数.java
+++ 非传统法找素数.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class Sushu {
 	public static void main(String[] args) {
-		final int top=300;
+//		final int top=300;
 		int x=2;
 		boolean [] numbers=new boolean [300];
-		for(int i1=0;i1<top;i1++)
+		for(int i1=0;i1<number.length;i1++)
 			numbers[i1]=true;
-		while(x<top){
-			for(int i=2;(x*i)<top;i++){
+		while(x<number.length){
+			for(int i=2;(x*i)<number.length;i++){
 				numbers[x*i]=false;
 			}//for
 			
 		x++;
 		}
-		for(int i=2;i<top;i++){
+		for(int i=2;i<number.length;i++){
 			if(numbers[i])
 				System.out.print(i+" ");
 		}
