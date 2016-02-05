3c4580c972ee1074b0d8e697006e0a66ac4b7510
葛尧 <geyao2012@gmail.com>
2016-02-05 10:14:31 +0800

    完整的时钟程序
    增加了秒的选项，了解了toString（）的特殊性

--- clock/src/clock/Clock.java
+++ clock/src/clock/Clock.java
@@ -3,20 +3,58 @@ package clock;
 public class Clock {
 	private Display hour = new Display (24);
 	private Display minute = new Display (60);
+	private Display second = new Display (60);
 	
+	public Clock( int hour, int minute, int second)
+	{
+		this.hour.setlimit(hour);
+		this.minute.setlimit(minute);
+		this.second.setlimit(second);
+	}
+	
+	public void tick()
+	{
+		if(this.second.valueincrease())
+		{
+			if(this.minute.valueincrease())
+			{
+				this.hour.valueincrease();
+			}
+		}
+	}
 	public void start()
 	{
 		while( true )
 		{
-			minute.valueincrease();
-			if( minute.value == 0 )
+			
+			if( minute.valueincrease() )
 				hour.valueincrease();
 			System.out.printf("%02d:%02d\n", hour.getvalue(), minute.getvalue() );
 		}
 	}
+	
+	public String toString()
+	{
+		String hour, minute, second;
+		if( this.hour.getvalue() < 10 )
+			hour = "0" + this.hour.getvalue();
+		else
+			hour = "" + this.hour.getvalue();
+		if( this.minute.getvalue() < 10 )
+			minute = "0" + this.minute.getvalue();
+		else 
+			minute = "" + this.minute.getvalue();
+		if( this.second.getvalue() < 10 )
+			second = "0" + this.second.getvalue();
+		else 
+			second = "" + this.second.getvalue();
+		
+		return hour + ":" + minute + ":" + second;
+	}
+	
 	public static void main( String[] args)
 	{
-		Clock clock = new Clock();
-		clock.start();
+		Clock c = new Clock ( 24, 60, 60);
+		System.out.println(c);
 	}
 }
--- clock/src/clock/Display.java
+++ clock/src/clock/Display.java
@@ -2,22 +2,35 @@ package clock;
 
 public class Display {
 	
-	int value = 0;
-	int limit = 0;
+	private int value = 0;
+	private int limit = 0;
+	
 	
 	Display( int a ){
 		this.limit = a;
 	}
-	void valueincrease(){
+	
+	public void setlimit( int limit)
+	{
+		this.limit = limit;
+	}
+	
+	public boolean valueincrease(){
+		boolean turn = false;
 		this.value ++;
 		if( this.value == this.limit )
+		{
 			this.value = 0;
+			turn = true;
+		}
+		return turn;
 	}
-	int getvalue(){
+	
+	public int getvalue(){
 		return value;
 	}
 	public static void main(String[] args) {
-		Display first = new Display ( 24 );
+//		Display first = new Display ( 24 );
 	}
 
 }
